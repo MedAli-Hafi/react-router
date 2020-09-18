f5b269c28a2a09dbdda910e2984e3fd17dd46aab
Mohamed Ali Hafi <hfdali123@gmail.com>
2020-09-18 16:53:20 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link,Route,Switch, Redirect } from 'react-router-dom';
+import Category from './Category';
+import Products from './Products';
+import  Login , { fakeAuth }  from './Login';
+function App() {
+  return (
+    <div className="med"  >
+      <nav>
+        <ul>
+        <li><Link to="/">Homes</Link></li>
+          <li><Link to="/category">Category</Link></li>
+          <li><Link to="/products">Products</Link></li>
+          <li><Link to="/admin">Admin area</Link></li>
+          
+        </ul>
+      </nav>
+      <Switch>
+      <Route path="/login" component={Login}/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/category" component={Category}/>
+        <PrivateRoute path='/admin' component = {Admin} />
+        <Route path="/products" component={Products}/>
+      </Switch>
+  </div>
+);
+  }
+ 
+//Private router function
+const PrivateRoute = ({component: Component, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => fakeAuth.isAuthenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />} />
+  )
+}
+
+//Home component
+const Home = (props) => (
+  <div>
+    <h2>Home {console.log(props)}</h2>
+  </div>
+)
+
+//Admin component
+const Admin = ({ match }) => {
+  return(<div> <h2>Welcome admin </h2></div>)
+
+
+}
+
+
+export default App;
